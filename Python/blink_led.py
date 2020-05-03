d493ce3ba8ca15373e732182b163dec23f0ef66c
Andrew Quamme <andrewquamme@gmail.com>
2020-05-02 23:34:35 -0700

    Python test GPIO, led, buzzer

--- Python/blink_led.py
+++ Python/blink_led.py
@@ -3,13 +3,13 @@ import time
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
 
 for i in range(1,11):
     print(i)
-    GPIO.output(18, True)
+    GPIO.output(26, True)
     time.sleep(1/i)
-    GPIO.output(18, False)
+    GPIO.output(26, False)
     time.sleep(1)
 
             
--- Python/buzzer.py
+++ Python/buzzer.py
@@ -8,16 +8,16 @@ GPIO.setwarnings(False)
 # Select GPIO mode
 GPIO.setmode(GPIO.BCM)
 
-# Set buzzer - pin 23 as output
+# Set buzzer - pin 
 buzzer=22
 
 GPIO.setup(buzzer,GPIO.OUT)
 
 # Run forever loop
-#while True:
-GPIO.output(buzzer,GPIO.HIGH)
-print ("Beep")
-sleep(0.5) # Delay in seconds
-GPIO.output(buzzer,GPIO.LOW)
-print ("No Beep")
-sleep(0.5)
+while True:
+    GPIO.output(buzzer,GPIO.HIGH)
+    print ("Beep")
+    sleep(0.5) # Delay in seconds
+    GPIO.output(buzzer,GPIO.LOW)
+    print ("No Beep")
+    sleep(0.5)
